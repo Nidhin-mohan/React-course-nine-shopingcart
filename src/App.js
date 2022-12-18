016ea31b12f61e61de5d5817a20d04d41dadb294
Nidhin-mohan <nidhinmohannidhin@gmail.com>
2022-12-18 08:39:09 +0530

    Project restart

--- src/App.js
+++ src/App.js
@@ -9,59 +9,63 @@ import './App.css';
 import { Container, Col, Row } from "reactstrap";
 import { ToastContainer,toast } from "react-toastify"
 
-import Cart from  "./Components/Cart"
-import BuyPage from "./Components/BuyPage";
+// import Cart from  "./Components/Cart"
+// import BuyPage from "./Components/BuyPage";
 
 
 
 function App() {
 
-  const [cartItem, setCartItem] = useState([]);
+//   const [cartItem, setCartItem] = useState([]);
 
-const addInCart = item => {
-  const isAlreadyAdded = cartItem.findIndex(function(array){
-      return array.id === item.id;
+// const addInCart = item => {
+//   const isAlreadyAdded = cartItem.findIndex(function(array){
+//       return array.id === item.id;
 
-  });
-  if(isAlreadyAdded !== -1) {
-    toast("already added in cart", {
-      type: "error"
-    });
-    return;
-  };
+//   });
+//   if(isAlreadyAdded !== -1) {
+//     toast("already added in cart", {
+//       type: "error"
+//     });
+//     return;
+//   };
 
-  setCartItem([...cartItem, item]);
-}
+//   setCartItem([...cartItem, item]);
+// }
 
-const buNow = () => {
-  setCartItem([])
+// const buyNow = () => {
+//   setCartItem([])
 
-  toast("purchase Complete", {
-    type: "success"
-  });
-};
+//   toast("purchase Complete", {
+//     type: "success"
+//   });
+// };
 
-const removeItem = item => {
-  setCartItem(cartItem.filter(singleItem => singleItem.id !== item.id))
-};
+// const removeItem = item => {
+//   setCartItem(cartItem.filter(singleItem => singleItem.id !== item.id))
+// };
 
 
 
   return (
    <Container>
- 
-    <ToastContainer>
+
+   <h1>hello world</h1>
+    {/* <ToastContainer>
+
       <Row>
-       
+       <h1>Working</h1>
         <Col md= "8">
         <BuyPage  addInCart = {addInCart} />
         </Col>
         <Col md= "4">
-          <Cart cartItem={cartItem}  removeItem={removeItem} buNow={buNow} />
+          <Cart cartItem={cartItem}  removeItem={removeItem} buNow={buyNow} />
         </Col>
       </Row>
-    </ToastContainer>
+
+    </ToastContainer> */}
    </Container>
+   
   );
 }
 
